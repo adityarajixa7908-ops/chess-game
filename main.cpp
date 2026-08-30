d40074664752df64e022078d250a979f1be1ff42
adityarajixa7908-ops <adityarajixa7908@gmail.com>
2026-08-30 21:58:40 +0530

    Create initial C++ chess game

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "================================\n";
+    cout << "          CHESS GAME\n";
+    cout << "================================\n";
+
+    return 0;
+}
